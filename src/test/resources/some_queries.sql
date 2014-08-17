6a18943f154f48b9937681875c87d2f7871ad198
RogerJFX <roger@crazything.de>
2014-08-17 16:38:00 +0200

    some example queries added to folder test/resources

---
+++ src/test/resources/some_queries.sql
@@ -0,0 +1,23 @@
+-- ################################################## -- 
+-- PostgreSQL only!
+-- After running the unit tests, any of the following queries should result in at least one row.
+-- ################################################## -- 
+
+-- Select all from tbl_typetest where field "iname" of itype (of type type_test) matches 'admin' 
+select * from tbl_typetest where (itype).iname='admin'
+
+-- Select all from tbl_typetest_single where field str of embedded type (type_test_int_string) matches 'foo'
+select * from tbl_typetest_single where ((itype).test_data).str='foo'
+
+-- Same with tbl_typetest, but only the first entry of array
+select * from tbl_typetest where ((itype).test_data[1]).str='foo'
+
+-- Search for a row in tbl_typetest where str in any array entry matches 'bar'
+select * from
+   (select id, (itype).test_data,
+           generate_subscripts((itype).test_data, 1) as s
+      from tbl_typetest) as foo
+ where test_data[s].str = 'bar';
+
+ -- Select str of first array entry (not index 0!!!)
+ select ((itype).test_data[1]).str from tbl_typetest
